cf423febf8d7b4be6b823b177792542faefd2d00
Njörd <davidbillamboz@gmail.com>
2019-11-05 13:13:56 +0100

    Implement markdown on 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 
-const NotFoundPage = () => (
+const NotFoundPage = ({ data }) => (
   <section className="hero is-fullheight-with-navbar">
     <div className="hero-body">
       <div className="container has-text-centered">
-        <div>la page demandée n’existe pas</div>
+        <div>{data.text}</div>
       </div>
     </div>
   </section>
 );
 
+NotFoundPage.propTypes = {
+  data: PropTypes.shape({
+    page: PropTypes.shape({
+      text: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
 export default NotFoundPage;
+
+export const pageQuery = graphql`
+  query {
+    page: markdownRemark(fields: { name: { eq: "page-404" } }) {
+      frontmatter {
+        metadata {
+          title
+          description
+        }
+        text
+      }
+    }
+  }
+`;
