c21f27950fe45d73bb8768cd5e383eaf868c0ba3
Njörd <davidbillamboz@gmail.com>
2019-12-10 20:32:02 +0100

    Fix theme module

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import theme from './src/styles/theme';
+import theme from './src/styles/theme.module.scss';
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>{element}</ThemeProvider>
