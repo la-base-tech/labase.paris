55a370ea7ae38e4bdc3bef92a25364c9452787d5
Njörd <davidbillamboz@gmail.com>
2020-02-07 00:34:54 +0100

    Disable purge on develop

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,7 +85,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
-        develop: true, // Activates purging in npm run develop
+        develop: false, // Activates purging in npm run develop
         printRejected: false,
       },
     },
