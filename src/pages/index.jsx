6edd81ca09817aa2cfe3ae6326224d5a16f9b1ce
Njörd <davidbillamboz@gmail.com>
2020-02-07 02:40:23 +0100

    Rename component folder

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import CrowdfundingSection from '../components/home-sections/CrowdfundingSection';
-import HeaderSection from '../components/home-sections/HeaderSection';
-import NewsletterSection from '../components/home-sections/NewsletterSection';
-import PresentationSection from '../components/home-sections/PresentationSection';
-import PresentationBisSection from '../components/home-sections/PresentationBisSection';
-import EventSection from '../components/home-sections/EventSection';
-import ActionSection from '../components/home-sections/ActionSection';
-import ObjectivesSection from '../components/home-sections/ObjectivesSection';
-import OrganisationsSection from '../components/home-sections/OrganisationsSection';
+import CrowdfundingSection from '../components/home-page/CrowdfundingSection';
+import HeaderSection from '../components/home-page/HeaderSection';
+import NewsletterSection from '../components/home-page/NewsletterSection';
+import PresentationSection from '../components/home-page/PresentationSection';
+import PresentationBisSection from '../components/home-page/PresentationBisSection';
+import EventSection from '../components/home-page/EventSection';
+import ActionSection from '../components/home-page/ActionSection';
+import ObjectivesSection from '../components/home-page/ObjectivesSection';
+import OrganisationsSection from '../components/home-page/OrganisationsSection';
 
 const IndexPage = () => (
   <>
