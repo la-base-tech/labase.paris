56104c1ddd04e323bd93b9340c40970d12777d2a
Njörd <davidbillamboz@gmail.com>
2020-03-18 02:24:15 +0100

    Fix fontawesome bug

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
 
 import '../styles/app.scss';
 
@@ -8,6 +13,8 @@ import SEO from './SEO';
 import Navbar, { Provider as NavbarProvider } from './Navbar';
 import Footer from './Footer';
 import { Provider as CrowdfundingStatusProvider } from './CrowdfundingStatus';
+
+config.autoAddCss = false; /* eslint-disable import/first */
 
 const Layout = ({ data, children, location, stats, crowdfunding }) => (
   <NavbarProvider>
